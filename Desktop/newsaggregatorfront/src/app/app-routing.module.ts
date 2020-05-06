c53ea0dbd530a8656b318502270997240b3a7221
vidhi-123 <vidhipshah10@gmail.com>
2020-05-06 14:34:42 +0530

    template done

---
+++ Desktop/newsaggregatorfront/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule, Component } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PostComponent } from './post/post.component';
+
+
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'post' , component: PostComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
